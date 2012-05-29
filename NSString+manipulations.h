50519065d99e8f729511397d2460b5c8aca14db0
Jim Geppert <jimgeppert@gmail.com>
2012-05-29 07:35:00 -0500

    cleanups

---
+++ NSString+manipulations.h
@@ -0,0 +1,13 @@
+//
+//  NSString+manipulations.h
+//  UltimateIPhone
+//
+//  Created by Jim Geppert on 5/29/12.
+//  Copyright (c) 2012 Summit Hill Software. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (manipulations)
+
+@end
---
+++ NSString+manipulations.m
@@ -0,0 +1,13 @@
+//
+//  NSString+manipulations.m
+//  UltimateIPhone
+//
+//  Created by Jim Geppert on 5/29/12.
+//  Copyright (c) 2012 Summit Hill Software. All rights reserved.
+//
+
+#import "NSString+manipulations.h"
+
+@implementation NSString (manipulations)
+
+@end

788d8142083faa2ff868313f94308d2bdc04b707
Jim Geppert <jimgeppert@gmail.com>
2012-10-08 20:01:09 -0500

    fix for "cancel" in back button

--- GameDetailViewController.m
+++ GameDetailViewController.m
@@ -65,6 +65,8 @@
 
 
 -(void)goToActionView {
+    UIBarButtonItem *backButton = [[UIBarButtonItem alloc] initWithTitle:@"Back" style:UIBarButtonItemStylePlain target:self action:@selector(popWithCurl)];
+    self.navigationItem.leftBarButtonItem = backButton;
     GameViewController* gameController = [[GameViewController alloc] init];
     [self.navigationController pushViewController:gameController animated:YES]; 
 }

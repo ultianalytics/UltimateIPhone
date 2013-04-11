2929ca8a664d22c6afef528f4e4f331530e660e7
Jim Geppert <jimgeppert@gmail.com>
2013-04-11 06:12:12 -0500

    Timeout WIP (nothing visible yet)

--- TimeoutViewController.m
+++ TimeoutViewController.m
@@ -7,15 +7,24 @@
 //
 
 #import "TimeoutViewController.h"
+#import "UltimateSegmentedControl.h"
+#import "StandardButton.h"
+#import "RedButton.h"
 
 @interface TimeoutViewController ()
+@property (strong, nonatomic) IBOutlet UltimateSegmentedControl *quotaPerHalfSegmentedControl;
+@property (strong, nonatomic) IBOutlet UltimateSegmentedControl *quotaFloatersSegmentedControl;
+@property (strong, nonatomic) IBOutlet UILabel *takenFirstHalf;
+@property (strong, nonatomic) IBOutletCollection(UILabel) NSArray *takenSecondHalf;
+@property (strong, nonatomic) IBOutletCollection(UILabel) NSArray *availableNow;
+@property (strong, nonatomic) IBOutlet StandardButton *timeoutButton;
+@property (strong, nonatomic) IBOutlet RedButton *undoButton;
 
 @end
 
 @implementation TimeoutViewController
 
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
-{
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *) nibBundleOrNil {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
     if (self) {
         // Custom initialization
@@ -23,16 +32,39 @@
     return self;
 }
 
-- (void)viewDidLoad
-{
+- (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view from its nib.
 }
 
-- (void)didReceiveMemoryWarning
-{
+- (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+}
+
+- (void)viewDidUnload {
+    [self setQuotaPerHalfSegmentedControl:nil];
+    [self setQuotaFloatersSegmentedControl:nil];
+    [self setTakenFirstHalf:nil];
+    [self setTakenSecondHalf:nil];
+    [self setAvailableNow:nil];
+    [self setTimeoutButton:nil];
+    [self setUndoButton:nil];
+    [super viewDidUnload];
+}
+
+- (IBAction)quotaPerHalfChanged:(id)sender {
+    
+}
+
+- (IBAction)floatersChanged:(id)sender {
+    
+}
+
+- (IBAction)timeoutButtonTapped:(id)sender {
+    
+}
+
+- (IBAction)undoButtonTapped:(id)sender {
+    
 }
 
 @end

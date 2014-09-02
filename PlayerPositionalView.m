ff084db0d0a72d194afc34dd196466d80109a462
Jim Geppert <jimgeppert@gmail.com>
2014-09-02 10:01:28 -0500

    Modal offense action chooser: (don't allow passer choosing...this is always done in earlier event)

--- PlayerPositionalView.m
+++ PlayerPositionalView.m
@@ -7,12 +7,21 @@
 //
 
 #import "PlayerPositionalView.h"
+#import "PasserButton.h"
 
 @implementation PlayerPositionalView
+
+
+#pragma mark - Superclass Overrides
 
 -(NSString*)nibName {
     return @"PlayerPositionalView";
 }
 
+-(void)initUI {
+    [super initUI];
+    // don't allow passer changes in the action view when collecting actions positionally
+    self.passerButton.userInteractionEnabled = NO;
+}
 
 @end

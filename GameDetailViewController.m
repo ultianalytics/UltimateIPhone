0aa3ccda987e5063b9779839a7ccf69fcc00a5b2
Jim Geppert <jimgeppert@gmail.com>
2014-12-12 09:47:48 -0800

    Updated version and turned off position so we can submit to store

--- GameDetailViewController.m
+++ GameDetailViewController.m
@@ -124,9 +124,9 @@
         [self.cells addObjectsFromArray:@[self.opponentRegularCell, self.tournamentCell]];
     }
     [self.cells addObjectsFromArray:@[self.initialLineCell, self.gamePointsCell]];
-    if (IS_IPAD) {
-        [self.cells addObject:self.positionalEventsCell];
-    }
+//    if (IS_IPAD) {
+//        [self.cells addObject:self.positionalEventsCell];
+//    }
     if (self.game.isPositional) {
         [self.cells addObjectsFromArray:@[self.fieldDimensionsCell]];
         if ([self.game hasEvents]) {
